b932085bf9c49ddb1c10d577fb1daa3123b46efa
@tbkoo53 <contact@webcoto.net>
2016-08-11 11:10:01 +0900

    2st commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,12 +5,19 @@
     <title>@yield('title')</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/4a5c519a7b.js"></script>
+    <link rel="stylesheet" href="{{ URL::to('src/css/app.css') }}">
     @yield('styles')
+
 </head>
 <body>
-    @yield('content')
+    @include('partials.header')
 
-            <!-- Latest compiled and minified JavaScript -->
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,6 +5,93 @@
 @endsection
 
 @section('content')
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <h1>It works!</h1>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 @endsection
