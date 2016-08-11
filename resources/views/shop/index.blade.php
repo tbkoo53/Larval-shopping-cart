6e982a96fe8655192bc52b101f765ad0ae1b25c6
@tbkoo53 <contact@webcoto.net>
2016-08-11 19:42:29 +0900

    7

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,93 +5,23 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+    @foreach($products->chunk(3) as $productChunk)
+        <div class="row">
+            @foreach($productChunk as $product)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{ $product->imagePath }}" alt="..." class="image-responsive">
+                        <div class="caption">
+                            <h3>{{ $product->title }}</h3>
+                            <p class="desc">{{ $product->description }}</p>
+                            <div class="clearfix">
+                                <div class="pull-left price">${{ $product->price }}</div>
+                                <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://t1.gstatic.com/images?q=tbn:ANd9GcTHxIgkw761_VLTV-5W6HhkCAsFChf6c97ELNfVjzHZpwcdx0Ps" alt="..." class="image-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsa voluptatum. Amet, autem, consectetur consequuntur facilis id ipsam molestias natus neque odit quas quibusdam, quidem quod quos soluta vel! Officia.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
+    @endforeach
 @endsection
