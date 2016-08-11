1444f8b86315dc4cb7d4dd00062baa99692d293a
@tbkoo53 <contact@webcoto.net>
2016-08-11 21:51:40 +0900

    11

--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.master')
+
+@section('content')
 <div class="row">
     <div class="col-md-4 col-md-offset-4">
         <h1>Sign Up</h1>
@@ -13,13 +16,14 @@
         <form action="" method="post">
             <div class="form-group">
                 <label for="email">E-Mail</label>
-                <input type="text" id="email" name="email">
+                <input type="text" id="email" name="email" class="form-control">
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" id="password" name="password">
+                <input type="password" id="password" name="password" class="form-control">
             </div>
-            <button type="submit">Sign Up</button>
+            <button type="submit" class="btn btn-primary">Sign Up</button>
         </form>
     </div>
 </div>
+@endsection
