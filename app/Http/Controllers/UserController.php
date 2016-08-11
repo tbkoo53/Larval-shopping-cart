632542a3ede74c542b8915e0051fe5362846f175
@tbkoo53 <contact@webcoto.net>
2016-08-11 22:33:08 +0900

    13

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Auth;
 
 class UserController extends Controller
 {
@@ -27,5 +28,25 @@ class UserController extends Controller
         $user->save();
 
         return redirect()->route('product.index');
+    }
+
+    public function getSignin() {
+        return view('user.signin');
+    }
+
+    public function postSignin(Request $request) {
+        $this->validate($request, [
+            'email' => 'email|required',
+            'password'=>'required|min:4'
+        ]);
+
+        if (Auth::attempt(['email' => $request->input('email'), 'password'=> $request->input('password') ])) {
+            return redirect()->route('user.profile');
+        }
+        return redirect()->back();
+    }
+
+    public function getProfile() {
+        return view('user.profile');
     }
 }
