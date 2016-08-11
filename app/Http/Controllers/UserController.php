74cd52f6063cdae0446710b67b3747cbd3c1f137
@tbkoo53 <contact@webcoto.net>
2016-08-11 21:44:11 +0900

    10

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UserController extends Controller
+{
+    public function getSignup() {
+        return view('user.signup');
+    }
+
+    public function postSignup(Request $request) {
+        $this->validate($request, [
+           'email' => 'email|required|unique:users',
+            'password'=>'required|min:4'
+        ]);
+
+        $user = new User([
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+
+        $user->save();
+
+        return redirect()->route('product.index');
+    }
+}
